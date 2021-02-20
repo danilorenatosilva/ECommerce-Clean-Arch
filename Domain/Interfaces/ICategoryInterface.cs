b1e894373dbb61ac1b0b94be50694f3371ec94e1
Danilo Renato da Silva <DaniloRenatoSilva@hotmail.com>
2021-02-20 12:23:06 -0300

    Adding Product and Category repository interfaces

---
+++ Domain/Interfaces/ICategoryInterface.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Domain.Interfaces
+{
+	public interface ICategoryInterface
+	{
+		IEnumerable<Category> GetCategories();
+		Category GetCategoryById(int id);
+		Category Add(Category category);
+		Category Update(int id, Category newCategory);
+		void Delete(int id);
+	}
+}
---
+++ Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Domain.Interfaces
+{
+	public interface IProductRepository
+	{
+		IEnumerable<Product> GetProducts();
+		Product GetProductById(int id);
+		Product Add(Product product);
+		Product Update(int id, Product newProduct);
+		void Delete(int id);
+	}
+}
