8939791645a2ca55f26382c57df0831bb1295279
Danilo Renato da Silva <DaniloRenatoSilva@hotmail.com>
2021-02-20 12:46:44 -0300

    Adding Infrastructure project and EF Context

---
+++ Infrastructure/Context/ECommerceDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context
+{
+	public class ECommerceDbContext : DbContext
+	{
+		public ECommerceDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<Product> Products { get; set; }
+		public DbSet<Category> Categories { get; set; }
+	}
+}
