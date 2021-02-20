304131de7300f953d79d87d7c3ba6b671f952b82
Danilo Renato da Silva <DaniloRenatoSilva@hotmail.com>
2021-02-20 00:28:31 -0300

    Adding Product and Category entities

---
+++ Domain/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+	public class Category
+	{
+		public int Id { get; set; }
+		public int? IdCategory { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string ImageUrl { get; set; }
+	}
+}
---
+++ Domain/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+	public class Product
+	{
+		public string Sku { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public decimal Price { get; set; }
+		public string ImageUrl { get; set; }
+		public Category Category { get; set; }
+	}
+}
